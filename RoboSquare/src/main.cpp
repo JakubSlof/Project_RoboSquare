3eb78a0e7ca02e64a3bc7eda90575dd077f37af0
Jakub Šlof <jakub.sloff@gmail.com>
2023-10-06 19:59:54 +0200

    adds baterry check function

--- RoboSquare/src/main.cpp
+++ RoboSquare/src/main.cpp
@@ -4,13 +4,26 @@
 const int wheel_diameter = 62;
 
 //to do upravit parametry
-void turn(int degrees,int speed){
-  rkMotorsDrive(100, PI * wheel_diameter * degrees/360 ,speed);
+//void turn(int degrees,int speed){
+//  rkMotorsDrive(100, PI * wheel_diameter * degrees/360 ,speed);
+//}
+void check_battery(){
+  int battery_percent = 0;
+  int battery_mid = 50;
+  int battery_low = 20;
+  battery_percent = rkBatteryPercent();
+  if(battery_percent > battery_mid){//if battery have more than 50% 
+    rkLedGreen(true); // turn on green LED
+  }
+  if((battery_percent > battery_low) && (battery_percent < battery_mid)){//if baterry have 20 - 50%
+    rkLedYellow(true); // turn on yellow LED
+  }
+  else{//if battery has less than 20%
+    rkLedRed(true); // turn on red LED
+  }
 }
 
-
 void setup() {
-
   rkConfig cfg;
   cfg.motor_max_power_pct = 100; // limit the power
   cfg.motor_wheel_diameter = wheel_diameter;
@@ -21,7 +34,5 @@ void setup() {
 }
 
 void loop() {
-   float command = Serial.parseFloat();// reads data from serial port 
-
 }
 
