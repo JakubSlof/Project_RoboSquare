22976e4b2fca5e9d571069cc55b357d506bdf0a5
Jakub Šlof <jakub.sloff@gmail.com>
2023-11-15 19:56:19 +0100

    o shit here we go again

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,33 @@
-#include "Robo_Square.h"
 #include<thread>
 #include<Arduino.h>
-RoboSquare RBS;
-
-
-void forward_f(int distance){
-    distance = distance * RBS.gears_ratio;
-    double left = 0;
-    double right =0;
-    rkMotorsSetSpeed(100,100);
-    while (left <= distance & right <= distance) 
-    {
-     left = rkMotorsGetPositionLeft();
-     right = rkMotorsGetPositionRight();
-     delay(1);
-    }
-    rkMotorsSetSpeed(0,0);  
-}
-
-void arc_f(double angle, double radius, int speed, int calibration, std::string side){
-    angle = angle + calibration;
-        double inner_length = (2 * PI * radius) / 360 * angle * RBS.gears_ratio;
-        double outer_lenght = (2 * PI * (RBS.wheel_base + radius)) / 360 * angle * RBS.gears_ratio;
-        int inner_speed = speed * (inner_length/outer_lenght);
-        int outer_speed = speed;
-    if(side == "left"){
-        double left = 0;
-        double right = 0;
-        rkMotorsSetSpeedLeft(inner_speed);
-        rkMotorsSetSpeedRight(outer_speed);
-        while (left <= inner_length & right <= outer_lenght) 
-            {
-            left = rkMotorsGetPositionLeft();
-            right = rkMotorsGetPositionRight();
-            delay(1);
-            }
-             rkMotorsSetSpeed(0,0);  
-        //rkMotorsDriveLeftAsync(inner_length, speed * (inner_length/outer_lenght), []() {Serial.print("Dojel jsem!\n");});
-        //rkMotorsDriveRightAsync(outer_lenght, speed, []() {Serial.print("Dojel jsem!\n");});
-    }
-    if(side == "right"){
-        double left = 0;
-        double right = 0;
-        rkMotorsSetSpeedLeft(outer_speed);
-        rkMotorsSetSpeedRight(inner_speed);
-        while (left <= outer_lenght & right <= inner_length) 
-            {
-            left = rkMotorsGetPositionLeft();
-            right = rkMotorsGetPositionRight();
-            delay(1);
-            }
-             rkMotorsSetSpeed(0,0);  
-        //rkMotorsDriveRightAsync(inner_length, speed*(inner_length/outer_lenght), []() {Serial.print("Dojel jsem!\n");});
-        //rkMotorsDriveLeftAsync(outer_lenght, speed, []() {Serial.print("Dojel jsem!\n");});
-    }
-    else{
-        Serial.println("error, used wrong argument");
-    }
-}
+#include "RBCX.h"
 
 void setup() {
-  rkConfig cfg;
-  cfg.motor_max_power_pct = 50; // limit the power
-  cfg.motor_wheel_diameter = RBS.wheel_diameter;
-  cfg.motor_max_ticks_per_second = 10000;
-  cfg.motor_max_acceleration = 17500;
-  rkSetup(cfg);
-  Serial.begin(115200);//starts serial communication
-  //std::thread thread1(&RoboSquare::check_battery, &RBS);// example of using thread 
-  RBS.check_battery();
-  //RBS.arc(80,250,20,"left");
-  //RBS.turn(80,100);//nefunguje
-  //RBS.forward(1000,80);//funguje 
-  //int count = 15;
-  // Serial.println("69");
-  // for (size_t i = 0; i < count; i++){
-  //    while (true){
-  //       int number = Serial.parseInt();
-  //       if (number==1){
-  //       break;
-  //       }
-  //    }
-  // start tlacitko
-    while (true) {
-        if (rkButtonIsPressed(BTN_UP)) {
-            break;
-        }
-    } 
-        //esko s oblouky
-    RBS.forward(500,100);
-    RBS.arc(180,150,100,0,"right");
-    delay(3000);
-    RBS.forward(100,100);
-    RBS.arc(180,150,100,-1,"left");
-    delay(3000);  
-    RBS.forward(1000,100);  
+auto& man = rb::Manager::get(); // get manager instance as singleton
+man.install(); // install manager
+int wheel_diameter = 72;
+double ticks_to_mm = (72*PI)/1796;//to do 
+   int32_t dist1 = 0;
+   int32_t dist4 = 0;
+   int32_t distance = 1780;
+   delay(1000);
+   Serial.println(ticks_to_mm);
+while(distance>dist1){
+man.motor(rb::MotorId::M1).speed(320);
+man.motor(rb::MotorId::M4).speed(-320);
+man.motor(rb::MotorId::M1).requestInfo([&dist1](rb::Motor& info) {
+            dist1 = info.position();
+            printf("M1: position:%d\n", info.position());
+        });
+man.motor(rb::MotorId::M4).requestInfo([&dist4,&ticks_to_mm](rb::Motor& info) {
+            dist4 = info.position();
+            dist4=-dist4;
+            auto distance = dist4*ticks_to_mm;
+            printf("M4: position:%d\n", dist4);
+            Serial.println(distance);
+        });  
+        delay(100);
+   }
 }
 
 void loop(){
