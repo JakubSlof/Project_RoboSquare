4ce1ad2ab5236482b07a497e92e67668f69c0d10
Jakub Šlof <jakub.sloff@gmail.com>
2023-11-15 21:24:53 +0100

    adds strait function

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include<thread>
 #include<Arduino.h>
 #include "RBCX.h"
-void drive_strait(){
-   auto& man = rb::Manager::get();
-   while(true){
-   man.motor(rb::MotorId::M1).speed(320);
-   man.motor(rb::MotorId::M4).speed(-320);
+double tics_to_mm = 300; // todo calculate the right value 
+
+void strait(int distance, int speed){
+   auto& man = rb::Manager::get(); //this needs to be there 
+
+   int tics1 = 0; //counter for ticks on motor 1
+   int tics4 = 0; //counter for ticks on motor 4
+   while(tics1<(distance*tics_to_mm) & tics4<(distance*tics_to_mm) ){ //checking if ticks on encoder is smaller than set value 
+   man.motor(rb::MotorId::M1).speed(speed); //setting motor 1 speed
+   man.motor(rb::MotorId::M4).speed(-speed); //setting motor 4 speed
+   man.motor(rb::MotorId::M4).requestInfo([&tics4](rb::Motor& info){
+         tics4 = info.position();
+         Serial.print("M4:");
+         Serial.println(tics4);
+   });
+   man.motor(rb::MotorId::M1).requestInfo([&tics1](rb::Motor& info){
+         tics1 = info.position();
+         Serial.print("M4:");
+         Serial.println(tics1);
+   });
    }
 }
 
@@ -15,7 +30,7 @@ void setup() {
 auto& man = rb::Manager::get(); // get manager instance as singleton
 man.install(); // install manager
 //int wheel_diameter = 72;
-drive_strait();
+strait(500,32767);
 double ticks_to_mm = (72*PI)/1796;//to do 
 ////////////////////////////////driving forward 
    int32_t dist1 = 0;
