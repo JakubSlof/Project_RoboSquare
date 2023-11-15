b7412590554ee4e8eb1540d48ac0ffa4f6f719d8
Jakub Šlof <jakub.sloff@gmail.com>
2023-11-15 21:09:11 +0100

    adds example funciton srive_strait

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,31 @@
 #include<thread>
 #include<Arduino.h>
 #include "RBCX.h"
+void drive_strait(){
+   auto& man = rb::Manager::get();
+   while(true){
+   man.motor(rb::MotorId::M1).speed(320);
+   man.motor(rb::MotorId::M4).speed(-320);
+   }
+}
+
 
 void setup() {
+
 auto& man = rb::Manager::get(); // get manager instance as singleton
 man.install(); // install manager
-int wheel_diameter = 72;
+//int wheel_diameter = 72;
+drive_strait();
 double ticks_to_mm = (72*PI)/1796;//to do 
+////////////////////////////////driving forward 
    int32_t dist1 = 0;
    int32_t dist4 = 0;
    int32_t distance = 1780;
-   delay(1000);
    Serial.println(ticks_to_mm);
 while(distance>dist1){
 man.motor(rb::MotorId::M1).speed(320);
 man.motor(rb::MotorId::M4).speed(-320);
+
 man.motor(rb::MotorId::M1).requestInfo([&dist1](rb::Motor& info) {
             dist1 = info.position();
             printf("M1: position:%d\n", info.position());
@@ -22,13 +33,16 @@ man.motor(rb::MotorId::M1).requestInfo([&dist1](rb::Motor& info) {
 man.motor(rb::MotorId::M4).requestInfo([&dist4,&ticks_to_mm](rb::Motor& info) {
             dist4 = info.position();
             dist4=-dist4;
-            auto distance = dist4*ticks_to_mm;
+            double distance = dist4*ticks_to_mm;
             printf("M4: position:%d\n", dist4);
-            Serial.println(distance);
-        });  
+            Serial.println(distance); 
+        }); 
+        
         delay(100);
    }
 }
+
+
 
 void loop(){
   //Serial.print("L: ");
