508a72bdd30e3499ef8639b903d9070605b1ed40
Jakub Šlof <jakub.sloff@gmail.com>
2023-12-13 19:09:00 +0100

    adds experiment version of deceleration

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include"RBCX.h"
 auto& man = rb::Manager::get(); //needs to be there to work man.sometning
 
-double tics_to_mm = 0.104;
+double mm_to_ticks = 0.104;
 int wheel_base = 170;
 int last_ticks_M4 = 0;
 int last_ticks_M1 = 0;
@@ -28,8 +28,8 @@ void UpdateLastTicks(){
 }
 void arc_right(int angle, int radius){
    auto& man = rb::Manager::get(); 
-   double inner_lenght = (((2*PI*radius)/360)*angle)/tics_to_mm;
-   double outer_lenght = (((2*3.14*(radius + wheel_base))/360)*angle) / tics_to_mm;
+   double inner_lenght = (((2*PI*radius)/360)*angle)/mm_to_ticks;
+   double outer_lenght = (((2*3.14*(radius + wheel_base))/360)*angle) / mm_to_ticks;
    Serial.println(inner_lenght);
    int outer_sped = 3200;
    int inner_speed = -(outer_sped*inner_lenght)/outer_lenght;
@@ -37,7 +37,7 @@ void arc_right(int angle, int radius){
    int tics_M1 = 0;
    int tics_M4 = 0;
    Serial.println(outer_lenght);
-       while ((inner_lenght > tics_M4) && (outer_lenght > tics_M1 ) ) { ////inner_lenght < (tics_M1*tics_to_mm)
+       while ((inner_lenght > tics_M4) && (outer_lenght > tics_M1 ) ) { ////inner_lenght < (tics_M1*mm_to_ticks)
        man.motor(rb::MotorId::M1).speed(outer_sped);
        man.motor(rb::MotorId::M4).speed(inner_speed);
        man.motor(rb::MotorId::M1).requestInfo([&tics_M1](rb::Motor& info) {
@@ -54,18 +54,25 @@ void arc_right(int angle, int radius){
        man.motor(rb::MotorId::M4).speed(0);
        UpdateLastTicks();
 }
-void Straight(int speed, int distance){
+void Straight(int speed, int distance,bool deceleration_enabled){
   double acceleration = 0.8;
+  double deceleration = 1;
   auto& man = rb::Manager::get(); 
    int ticks_M1 = 0;
    int ticks_M4 = 0;
-   distance = distance / tics_to_mm;     
+   distance = distance / mm_to_ticks;
+   int deceleration_distance = 100/mm_to_ticks;
+   
+   
   while((ticks_M1 < distance) && (ticks_M4 < distance)){
-    if(acceleration<100){
-    acceleration = acceleration + 0.01;}
-
-    man.motor(rb::MotorId::M1).speed(speed*acceleration);
-    man.motor(rb::MotorId::M4).speed(-speed*acceleration);
+    if(acceleration<1){
+    acceleration = acceleration + 0.02;}
+    if(deceleration_enabled == true && (ticks_M1 > (distance-deceleration_distance)) && (ticks_M4 > (distance-deceleration_distance))&&deceleration<1){
+      deceleration = deceleration - 0.05;
+    
+   }
+    man.motor(rb::MotorId::M1).speed(speed*acceleration*deceleration);
+    man.motor(rb::MotorId::M4).speed(-speed*acceleration*deceleration);
 
     man.motor(rb::MotorId::M4).requestInfo([&ticks_M4](rb::Motor& info) {
             printf("M4: position:%d\n", info.position());
@@ -93,8 +100,8 @@ void Straight(int speed, int distance){
  */
 void arc_left(int angle, int radius){
    auto& man = rb::Manager::get(); 
-   double inner_lenght = (((2*PI*radius)/360)*angle)/tics_to_mm;
-   double outer_lenght = (((2*3.14*(radius + wheel_base))/360)*angle) / tics_to_mm;
+   double inner_lenght = (((2*PI*radius)/360)*angle)/mm_to_ticks;
+   double outer_lenght = (((2*3.14*(radius + wheel_base))/360)*angle) / mm_to_ticks;
    Serial.println(inner_lenght);
    int outer_sped = -3200;
    int inner_speed = -(outer_sped*inner_lenght)/outer_lenght;
@@ -102,7 +109,7 @@ void arc_left(int angle, int radius){
    int tics_M1 = 0;
    int tics_M4 = 0;
    Serial.println(outer_lenght);
-       while ((inner_lenght > tics_M1) && (outer_lenght > tics_M4 ) ) { ////inner_lenght < (tics_M1*tics_to_mm)
+       while ((inner_lenght > tics_M1) && (outer_lenght > tics_M4 ) ) { ////inner_lenght < (tics_M1*mm_to_ticks)
        man.motor(rb::MotorId::M4).speed(outer_sped);
        man.motor(rb::MotorId::M1).speed(inner_speed);
        man.motor(rb::MotorId::M4).requestInfo([&tics_M4](rb::Motor& info) {
@@ -130,11 +137,11 @@ void setup() {
   
   // Set the serial communication baud rate to 115200
   Serial.begin(115200);
-  Straight(3200, 500);
+  Straight(3200, 500,true);
   arc_right(170, 150);
-  Straight(3200, 150);
+  Straight(3200, 150,true);
   arc_left(150, 150);
-  Straight(3200, 1000);
+  Straight(3200, 1000,true);
   // arc_left(180,200);
   // delay(100);
   // UpdateLastTicks();
